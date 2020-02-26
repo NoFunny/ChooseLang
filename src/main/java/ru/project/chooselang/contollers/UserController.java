1d0960154b67839d8f991716e024c520a78206be
NoFunny <chunikhin_1999@mail.ru>
2020-02-26 20:40:25 +0700

    Add user controller, revision user service, web controller and security config

---
+++ src/main/java/ru/project/chooselang/contollers/UserController.java
@@ -0,0 +1,37 @@
+package ru.project.chooselang.contollers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ru.project.chooselang.entity.User;
+import ru.project.chooselang.services.UserService;
+import ru.project.chooselang.utils.SplitURL;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+
+    @RequestMapping(value = "/add_user", method = RequestMethod.POST)
+    public Byte add_user(@RequestBody String object) throws UnsupportedEncodingException {
+        byte answer = 0;
+        ArrayList<String> req = SplitURL.split(object);
+        if (req.size() == 5) {
+            User user = new User(req.get(0), req.get(1), req.get(2), req.get(3), req.get(4));
+            answer = userService.AddUser(user);
+            return answer;
+        } else {
+//            User user = new User(req.get(0), req.get(1), req.get(2), req.get(3), req.get(4));
+//            byte answer = userService.AddUser(user);
+            return answer;
+        }
+    }
+}
--- src/main/java/ru/project/chooselang/services/UserService.java
+++ src/main/java/ru/project/chooselang/services/UserService.java
@@ -4,16 +4,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.Md4PasswordEncoder;
+import org.springframework.stereotype.Service;
 import ru.project.chooselang.dao.UserRepository;
+import ru.project.chooselang.entity.*;
 
+import javax.transaction.Transactional;
+import java.util.Collections;
+
+@Service
 public class UserService implements UserDetailsService {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
+    @Transactional
+    public byte AddUser(User user) {
+        if(userRepository.existsByEmail(user.getEmail())) {
+            System.out.println(user.getEmail());
+            return 1;
+        }
 
+        if(userRepository.existsByUsername(user.getUsername())) {
+            System.out.println(user.getUsername());
+            return 2;
+        }
+
+        if(userRepository.existsByUsernameAndEmail(user.getUsername(), user.getEmail())) {
+            System.out.println("Username = " + user.getUsername() + "||| Email = " + user.getEmail());
+            return 3;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+        return 0;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Username = " + username);
         return userRepository.findByUsername(username);
     }
 }
